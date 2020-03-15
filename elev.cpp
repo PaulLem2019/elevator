f45737728cbce2eeff379919b694954aa57ebe88
user <user@acer_lin.home>
2020-03-15 11:15:10 +0300

    probes put repository to github

---
+++ elev.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include "elev.h"
+
+Elev::Elev(int floor)
+{
+	ElevMove(floor);
+}
+
+int Elev::ElevMove(int ToFloor1)
+{
+	ToFloor=ToFloor1;
+	
+	if (ToFloor > floor)
+	{
+		move=1;
+	}
+	else
+	{
+		if(ToFloor < floor)
+		{
+			move = -1;
+		}
+		else
+		{
+			move = 0;
+		}
+	}
+	return ToFloor;
+}
+
+int Elev::ElevFloor()
+{
+	if(ToFloor!=floor)
+	{
+		floor += move;
+	}
+	
+	return floor;
+}
+
+int main()
+{
+	Elev Elevator;
+	
+	Elevator.ElevMove(5);
+	
+	
+	for(int i=0;i<6;i++)
+	{
+		std::cout << Elevator.ElevFloor() << std::endl;
+	}
+	
+	
+	return 0;
+}
---
+++ elev.h
@@ -0,0 +1,16 @@
+﻿#ifndef _elev_h_
+#define _elev_h_
+
+class Elev
+{
+	public:
+	Elev (int=0);
+	int ElevMove(int=0);
+	int ElevFloor();
+	private:
+	int floor=0;
+	int move =0;
+	int ToFloor=0;
+};
+
+#endif
